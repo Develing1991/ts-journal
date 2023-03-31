17f99da3c1be48618d9b4a6e0b54efffb9a53fb6
Develing1991 <completed0728@gmail.com>
2023-03-31 11:33:18 +0900

    event typing

--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -4,21 +4,29 @@ import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
 import { ref, computed } from "vue";
 import type Emoji from "@/types/Emoji";
 const text = ref("");
-
 const emoji = ref<Emoji | null>(null);
-
-// const charCount = computed<number>(() => text.value.length);
 const charCount = computed(() => text.value.length);
+const maxChars = 280;
+const handleTextInput = (e: KeyboardEvent) => {
+  const textarea = e.target as HTMLTextAreaElement;
+  console.log(textarea.value);
+  if (textarea.value.length <= maxChars) {
+    text.value = textarea.value;
+  } else {
+    text.value = textarea.value = textarea.value.substring(0, maxChars);
+  }
+};
 </script>
 <template>
   <form class="entry-form" @submit.prevent>
     <textarea
-      v-model="text"
+      :value="text"
+      @keyup="handleTextInput"
       placeholder="New Journal Entry for danielkelly_io"
     ></textarea>
     <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>{{ charCount }} / 280</span>
+      <span>{{ charCount }} / {{ maxChars }}</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
     </div>
   </form>
