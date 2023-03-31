63b98c38183375660e155791f86d087e243ff37a
Develing1991 <completed0728@gmail.com>
2023-03-31 12:37:40 +0900

    emit event typing

--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -3,10 +3,29 @@ import EmojiField from "@/components/EmojiField.vue";
 import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
 import { ref, computed } from "vue";
 import type Emoji from "@/types/Emoji";
+import type Entry from "@/types/Entry";
+
+// datas
 const text = ref("");
 const emoji = ref<Emoji | null>(null);
 const charCount = computed(() => text.value.length);
 const maxChars = 280;
+
+// events
+// defineEmits<{
+//   (
+//     e: "@create",
+//     entry: {
+//       text: string;
+//       emoji: Emoji | null;
+//     }
+//   ): void;
+// }>();
+defineEmits<{
+  (e: "@create", entry: Entry): void;
+}>();
+
+// methods
 const handleTextInput = (e: KeyboardEvent) => {
   const textarea = e.target as HTMLTextAreaElement;
   console.log(textarea.value);
@@ -18,11 +37,22 @@ const handleTextInput = (e: KeyboardEvent) => {
 };
 </script>
 <template>
-  <form class="entry-form" @submit.prevent>
+  <form
+    class="entry-form"
+    @submit.prevent="
+      $emit('@create', {
+        body: text,
+        emoji,
+        createdAt: new Date(),
+        userId: 1,
+        id: Math.random(),
+      })
+    "
+  >
     <textarea
       :value="text"
-      @keyup="handleTextInput"
       placeholder="New Journal Entry for danielkelly_io"
+      @keyup="handleTextInput"
     ></textarea>
     <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
